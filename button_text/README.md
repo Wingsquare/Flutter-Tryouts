3b721d56e29759f801574ffb0d954bb3fc3a7419
georgemathewk <georgemathewk@yahoo.com>
2021-05-28 12:18:30 +0530

    Deleted the project button_text

--- button_text/README.md
+++
@@ -1,3 +0,0 @@
-# button_text
-
-Change text in Text widget on button press of elevated button
--- button_text/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:button_text/btnText.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(BtnText());
-}
